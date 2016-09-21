ea9b11bbf84f1172c2cd29259f9780b4c38e6663
ljq8896 <ljq8896@gmail.com>
2016-09-21 08:01:52 +0800

    build160921

---
+++ Project1/server.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+using namespace std;
+int main(void)
+{
+	int str;
+	cin >> str;
+	
+	cout << "your input is " << str << endl;
+	system("pause");
+	return 0;
+}
--- Project1/源.cpp
+++
@@ -1,8 +0,0 @@
-#include "iostream"
-
-int main(void)
-{
-	std::cout << "hello" << std::endl;
-	system("pause");
-	return 0;
-}
